1828792d4f5d04bff56e6878589f9e6a28d1423f
Steffen Karlsson <steffenkarlsson@steffenkarlssons-MacBook-Pro.local>
2015-03-01 22:15:03 +0100

    final solution

--- Assignment 2/src.py
+++ Assignment 2/src.py
@@ -1,7 +1,10 @@
 import matplotlib.pyplot as plt
 
-from numpy import array, vstack, mean, dot, sqrt
-from numpy.linalg import pinv
+from numpy import array, vstack, mean, dot, sqrt, where, loadtxt, cov, add, log
+from numpy import identity, transpose, linspace, concatenate
+from numpy.linalg import pinv, inv
+
+BETA = 1
 
 
 def load_data(data_file, columns):
@@ -24,37 +27,162 @@ def calculate_wml(design_matrix, t):
     return dot(pinv(design_matrix), t)
 
 
-def plot_linear_regression_2d(wml, expected_t, actual_t):
-    y = sum(actual_t, [])
-    x = expected_t.flatten()
-    plt.scatter(x, y, c='b', marker='o')
-    plt.plot(x, eval('{0}+{1}*x'.format(wml[0][0], wml[1][0])), c='r')
+def plot_linear_regression_2d(train_data, train_actual, test_data, test_actual, wml):
+    test_data = array(test_data).flatten()
+    train_actual = array(train_actual).flatten()
+    train_data = array(train_data).flatten()
+
+    plt.scatter(train_data, train_actual, c='b', marker='o')
+    plt.scatter(test_data, test_actual, c='g', marker='x')
+    plt.plot(test_data, eval('{0}+{1}*test_data'.format(wml[0][0], wml[1][0])), c='r')
     plt.show()
 
 
-def calculate_expected_target(data):
-    target = []
-    for inx in xrange(0, len(data)):
-        target.append([mean(data[inx])])
-    return array(target)
+def rms(data, t, w):
+    def __y(_data, _w):
+        return dot(_w.T, array([1] + _data))
+
+    return sqrt(sum((t[i] - __y(d, w))**2 for i, d in enumerate(data)) / len(data))
 
 
-def rms(data, actual_target, wml):
-    return sqrt(sum([(actual_target[idx][0] - y(val, wml)) ** 2 for idx, val in enumerate(data)]) / len(data))
+def MAP(dm, t, alpha, beta=1):
+    map_cov = inv(alpha * identity(len(dm[0])) + beta * dot(transpose(dm), dm))
+    return beta * dot(dot(map_cov, transpose(dm)), t)
 
 
-def y(x, w):
-    return (w[0] + dot(x, w[1:]))[0]
+def split_into_species(data):
+    sorted_data = array(sorted(data, key=lambda entry: entry[2]))
+    species_indicies = sorted_data[:, 2]
+
+    inx1 = where(species_indicies == 1)[0][0]
+    inx2 = where(species_indicies == 2)[0][0]
+
+    return sorted_data[:inx1, 0:2], sorted_data[inx1:inx2, 0:2], sorted_data[inx2:, 0:2]
+
+
+def calculate_cov(species0, species1, species2):
+    covs = array([cov(transpose(species0)), cov(transpose(species1)), cov(transpose(species2))])
+    return add(*covs) / (200 - len(covs))
+
+
+def lda(x, species, sigma, combined_sum=200.0):
+    mean_species = mean(species, axis=0)
+    prior = log(len(species) / combined_sum)
+    inv_sigma = inv(sigma)
+    return dot(transpose(x), dot(inv_sigma, mean_species)) \
+           - 0.5 * dot(dot(transpose(mean_species), inv_sigma), mean_species) + prior
+
+
+def best_category(x, all_species):
+    sigma = calculate_cov(*all_species)
+    ldas = [lda(x, species, sigma) for species in all_species]
+    return ldas.index(max(ldas))
+
+
+def plot_train_test_data(train, test, plot_edge=False):
+    def __get_species_color(s):
+        if s == 0:
+            return 'red'
+        elif s == 1:
+            return 'blue'
+        else:
+            return 'orange'
+
+    for len, width, species in train:
+        plt.scatter(len, width, color=__get_species_color(species))
+
+    if test is not None:
+        for len, width, species in test:
+            if plot_edge:
+                plt.scatter(len, width, color='grey', edgecolors=__get_species_color(species))
+            else:
+                plt.scatter(len, width, color='grey')
+    plt.show()
+
+
+def lost_function(test, new_test):
+    count = 0
+    for idx, val in enumerate(test):
+        new_val = new_test[idx]
+        if new_val[2] != val[2]:
+            count += 1
+    return count / float(len(test))
+
+
+def normalize(data):
+    lens, heights, species = zip(*data)
+    sum_lens = sum(lens)
+    sum_heights = sum(heights)
+
+    lens = [val / sum_lens for val in lens]
+    heights = [val / sum_heights for val in heights]
+    return array([array([lens[i], heights[i], species[i]]) for i in xrange(len(species))])
+
+
+# II.1.1
+test = loadtxt("IrisTest2014.dt")
+train = loadtxt("IrisTrain2014.dt")
+
+all_species = split_into_species(train)
+new_train = train.copy()
+for data in new_train:
+    data[2] = best_category(data[0:2], all_species)
+plot_train_test_data(train, new_train, plot_edge=True)
+print str(lost_function(train, new_train))
+
+
+new_test = test.copy()
+for data in new_test:
+    data[2] = best_category(data[0:2], all_species)
+plot_train_test_data(train, new_test, plot_edge=True)
+print str(lost_function(test, new_test))
+
+
+# II.1.2
+normalized_data = normalize(concatenate((train, test)))
+normalized_train = normalized_data[:len(train)]
+normalized_test = normalized_data[len(train):]
+
+normalized_all_species = split_into_species(normalized_train)
+new_test = normalized_test.copy()
+
+for data in new_test:
+    data[2] = best_category(data[0:2], normalized_all_species)
+plot_train_test_data(normalized_train, new_test, plot_edge=True)
+print str(lost_function(normalized_test, new_test))
 
 
 # II.2.1
-actual_t = load_data("sunspotsTrainStatML.dt", [6])
+train_actual_t = load_data("sunspotsTrainStatML.dt", [6])
+test_actual_t = load_data("sunspotsTestStatML.dt", [6])
+
 for idx, columns in enumerate([[3, 4], [5], range(1, 6)]):
-    data = load_data("sunspotsTrainStatML.dt", columns)
-    expected_t = calculate_expected_target(data)
-    dm = design_matrix_linear_regression(data)
-    wml = calculate_wml(dm, array(expected_t))
-    print "RMS: " + str(rms(data, actual_t, wml))
+    train_data = load_data("sunspotsTrainStatML.dt", columns)
+    dm = design_matrix_linear_regression(train_data)
+    wml = calculate_wml(dm, array(train_actual_t))
+    test_data = load_data("sunspotsTestStatML.dt", columns)
+
+    print "RMS: " + str(rms(test_data, test_actual_t, wml))
     if idx == 1:
-        plot_linear_regression_2d(wml, expected_t, actual_t)
+        plot_linear_regression_2d(train_data, train_actual_t, test_data, test_actual_t, wml)
     print wml
+
+
+# II.2.2
+for idx, columns in enumerate([[3, 4], [5], range(1, 6)]):
+    train_data = load_data("sunspotsTrainStatML.dt", columns)
+    test_data = load_data("sunspotsTestStatML.dt", columns)
+    dm = design_matrix_linear_regression(train_data)
+
+    rmss = []
+    alphas = linspace(0, 5000, 2000)
+    for alpha in alphas:
+        pm = MAP(dm, train_actual_t, alpha)
+        rmss.append(rms(test_data, test_actual_t, pm))
+
+    optimal_alpha = alphas[rmss.index(min(rmss))]
+    print "Optimal Alpha: " + str(optimal_alpha)
+    print "RMS: " + str(min(rmss))
+    plt.plot(alphas, rmss, c='b')
+    plt.axis([0, 5000, 10, 60])
+    plt.show()
