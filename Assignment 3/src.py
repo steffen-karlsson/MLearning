44adc0585ca4d0abfc55aef11d9aa408766d9c8d
Steffen Karlsson <steffenkarlsson@steffenkarlssons-MacBook-Pro.local>
2015-03-17 09:42:35 +0100

    svm and gradient verify

--- Assignment 3/src.py
+++ Assignment 3/src.py
@@ -1,16 +1,32 @@
 import matplotlib.pyplot as plt
 
-from numpy import loadtxt, array, append, sqrt, inf, arange, sin, round, average, subtract, dot, add, newaxis
+from numpy import loadtxt, array, sqrt, sin, average, zeros
+from numpy import subtract, dot, add, mean, var, arange
 from numpy.random import rand, seed, sample
 
-pTest = loadtxt("data/parkinsonsTestStatML.dt")
-pTrain = loadtxt("data/parkinsonsTrainStatML.dt")
+from copy import copy
+
+from sklearn.svm import SVC
+from sklearn.grid_search import GridSearchCV
+
+pTestData = loadtxt("data/parkinsonsTestStatML.dt", usecols=tuple(range(0, 21)))
+pTestTarget = loadtxt("data/parkinsonsTestStatML.dt", usecols=(22, ))
+pTrainData = loadtxt("data/parkinsonsTrainStatML.dt", usecols=tuple(range(0, 21)))
+pTrainTarget = loadtxt("data/parkinsonsTrainStatML.dt", usecols=(22, ))
+
 sTrainData = loadtxt("data/sincTrain25.dt", usecols=(0, ))
 sTrainTarget = loadtxt("data/sincTrain25.dt", usecols=(1, ))
 sValidateData = loadtxt("data/sincValidate10.dt", usecols=(0, ))
 sValidateTarget = loadtxt("data/sincValidate10.dt", usecols=(1, ))
 
 seed(10)
+
+num_hidden = 2
+weights_md = sample([num_hidden, 2])
+weights_km = sample([1, num_hidden + 1])
+trainInterval = arange(-10, 10, 0.05, dtype='float64')
+learning_rates = [0.0001, 0.001, 0.01, 0.1, 1, 10, 100]
+
 
 def input_unit(a):
     return a
@@ -69,17 +85,17 @@ def delta_j(aj, weight, delta_k):
     return alt_sigmoid_prime(aj) * sum([weight * data_k for k, data_k in enumerate(delta_k)])
 
 
-def back_prop(steps, learning_rate, weights_md, weights_km):
+def back_prop(train, target, steps, learning_rate, weights_md, weights_km):
     errors = []
 
     for _ in xrange(steps):
-        estimated = nn(sTrainData, weights_md, weights_km, num_hidden)
+        estimated = nn(train, weights_md, weights_km, num_hidden)
         estimated_interval = nn(trainInterval, weights_md, weights_km, num_hidden)
 
         dhiddens = []
         douts = []
-        for i, data in enumerate(sTrainData):
-            dK = delta_k(estimated[i], sTrainTarget[i])
+        for i, data in enumerate(train):
+            dK = delta_k(estimated[i], target[i])
 
             dJs = []
             list_zj = []
@@ -92,30 +108,142 @@ def back_prop(steps, learning_rate, weights_md, weights_km):
             list_zj.append(array([alt_sigmoid(1)]))
             douts.append(dK * array(list_zj))
 
-        weights_md = subtract(weights_md, (learning_rate * average(dhiddens, axis=0)))
-        weights_km = subtract(weights_km, [list(average(douts, axis=0).flatten())])
+        avg_dhidden = average(dhiddens, axis=0)
+        avg_dout = [list(average(douts, axis=0).flatten())]
+        weights_md = subtract(weights_md, (learning_rate * avg_dhidden))
+        weights_km = subtract(weights_km, avg_dout)
 
-        errors.append(MSE(estimated, sTrainTarget))
-    return errors, estimated_interval
+        errors.append(MSE(estimated, target))
+    return errors, estimated_interval, avg_dhidden, avg_dout
 
 
-num_hidden = 5
-weights_md = sample([num_hidden, 2])
-weights_km = sample([1, num_hidden + 1])
+def standardization(data, features=None):
+    updated_data = []
+    new_features = []
+    data = zip(*data)
 
-trainInterval = arange(-10, 10, 0.05, dtype='float64')
-errors, data = back_prop(10000, 0.1, weights_md, weights_km)
-# data = nn(trainInterval, wMD, wKM, num_hidden)
+    # print "Mean (Data): " + str(mean(data, axis=1))
+    # print "Variance (Data): " + str(var(data, axis=1))
 
-print MSE(data, eval('sin(trainInterval)/trainInterval'.format(trainInterval)))
+    for idx, d in enumerate(data):
+        if features:
+            d_var = features[idx][0]
+            d_mean = features[idx][1]
+        else:
+            d_var = sqrt(var(d))
+            d_mean = mean(d)
+            new_features.append((d_var, d_mean))
+        updated_data.append(array([(entry - d_mean) / d_var for entry in d]))
+
+    updated_data = array(updated_data)
+    # print "Mean (Norm data): " + str(mean(updated_data, axis=1))
+    # print "Variance (Norm data): " + str(var(updated_data, axis=1))
+
+    return updated_data.T, new_features
+
+
+def estimate_svm_params(data, target, c_range, y_range):
+    svr = SVC(kernel='rbf')
+    params = {'C': c_range, 'gamma': y_range}
+    clf = GridSearchCV(svr, params, cv=5)
+    clf.fit(data, target)
+    best_params = clf.best_params_
+    return best_params['C'], best_params['gamma']
+
+
+def svm(data, target, c, y):
+    svr = SVC(kernel='rbf', C=c, gamma=y)
+    svr.fit(data, target)
+    return svr
+
+
+def svm_classify(svr, data):
+    return svr.predict(data)
+
+
+def gradient_verify(data, data_target, weight_km, weight_md, e):
+    error_matrix_md = zeros(weights_md.shape)
+    error_matrix_km = zeros(weights_km.shape)
+
+    errors, _, _, _ = back_prop(data, data_target, 1, -1, weights_md, weights_km)
+
+    for i in xrange(weights_md.shape[1]):
+        for j in xrange(len(weights_md)):
+            cpy_weight_md = copy(weights_md)
+            cpy_weight_md[j][i] += e
+            e_errors, _, _, _ = back_prop(data, data_target, 1, -1, cpy_weight_md, weights_km)
+            error_matrix_md[i][j] = (e_errors[0][0] - errors[0][0]) / e
+
+    for i in xrange(len(weights_km)):
+        cpy_weight_km = copy(weights_km)
+        cpy_weight_km[i] += e
+        e_errors, _, _, _ = back_prop(data, data_target, 1, -1, weight_md, cpy_weight_km)
+        error_matrix_km[i] = (e_errors[0][0] - errors[0][0]) / e
+    return error_matrix_md, error_matrix_km
+
+
+def lost_function(test, new_test):
+    count = 0
+    for idx, val in enumerate(test):
+        if new_test[idx] != val:
+            count += 1
+    return count / float(len(test))
+
+
+def one_loss_svm_classification(train_data, train_target,
+                                test_data, test_target):
+    c_range = arange(9, 11.01, 0.01)
+    y_range = [0.001, 0.01, 0.1, 1, 10, 100]
+    c_best, y_best = estimate_svm_params(train_data, train_target,
+                                         c_range, y_range)
+    print "Best Hyperparameters (C, y): (%.3f , %.3f)" % (c_best, y_best)
+    _svm = svm(train_data, train_target, c_best, y_best)
+
+    _classification = svm_classify(_svm, train_data)
+    print "Loss function for training data: " + str(lost_function(train_target, _classification))
+
+    _classification = svm_classify(_svm, test_data)
+    print "Loss function for test data: " + str(lost_function(test_target, _classification))
+
+## III.1
+
+error_matrix_md, error_matrix_km = gradient_verify(sTrainData[:10], sTrainTarget[:10], weights_km, weights_md, 0.0000000000001)
+_, _, avg_dhidden, avg_dout = back_prop(sTrainData[:10], sTrainTarget[:10], 1, -1, weights_md, weights_km)
+
+print subtract(error_matrix_md, avg_dhidden)
+print subtract(error_matrix_km, avg_dout)
+
+errors_list = []
+data_list = []
+
+for learning_rate in learning_rates:
+    errors, data, _, _ = back_prop(sTrainData, sTrainTarget,
+                                   10000, learning_rate, weights_md, weights_km)
+    errors_list.append(errors)
+    data_list.append(data)
 
 plt.gca().set_yscale('log')
 
-plt.plot(range(len(errors)), errors)
+for index, err in enumerate(errors_list):
+    plt.plot(range(len(err)), err, label="Learning rate " + str(learning_rates[index]))
+plt.legend()
 plt.show()
 
-plt.plot(trainInterval, data)
-plt.plot(trainInterval, eval('sin(trainInterval)/trainInterval'.format(trainInterval)))
+for index, da in enumerate(data_list):
+    plt.plot(trainInterval, da, label="Learning rate " + str(learning_rates[index]))
+plt.plot(trainInterval, eval('sin(trainInterval)/trainInterval'.format(trainInterval)), label="sin(x)/x")
 plt.scatter(sTrainData, sTrainTarget)
+plt.legend()
 plt.show()
 
+
+## III.2.1
+print "Data:"
+norm_pTrainData, features = standardization(pTrainData)
+one_loss_svm_classification(pTrainData, pTrainTarget,
+                            pTestData, pTestTarget)
+
+print "\nNormalized data:"
+norm_pTestData, _ = standardization(pTestData, features=features)
+one_loss_svm_classification(norm_pTrainData, pTrainTarget,
+                            norm_pTestData, pTestTarget)
